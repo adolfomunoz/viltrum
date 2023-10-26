71d0eec0f526e90523ae85bf7ed9fe2c74d637d9
Adolfo <adolfo@unizar.es>
2023-10-26 13:35:48 +0200

    Progress logger is reasonable.

--- src/log.h
+++ src/log.h
@@ -22,13 +22,15 @@ public:
     LoggerProgress(const std::string& n) : name(n) {}
     template<typename Number>
     void log_progress(const Number& number, const Number& last = Number(1)) {
+        static Number prev_number(0);
         if (number<=Number(0)) {
             start = std::chrono::steady_clock::now();
             std::cerr<<name<<" -                           \r";
-        } else {
+        } else if (std::abs(double(number)-double(prev_number))>(double(last)*0.01)) {
             auto elapsed = std::chrono::duration_cast<std::chrono::duration<double> >(std::chrono::steady_clock::now() - start);
             std::cerr<<name<<" - \t"<<std::fixed<<std::setprecision(2)<<std::setw(6)<<(100.0f*float(number)/float(last))<<"%\t("<<std::setprecision(3)<<std::setw(6)<<elapsed.count()<<" seconds)\r";
-        }              
+            prev_number=number;
+        }  
     }
     template<typename Data>
     void log(const Data& data) {}
