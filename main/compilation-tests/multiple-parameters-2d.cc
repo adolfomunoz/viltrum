748e995e267484c80b361b33bc4fc5473d0210f4
Adolfo <adolfo@unizar.es>
2024-01-31 14:25:51 +0100

    Added new test, parallel 2D.

--- main/compilation-tests/multiple-parameters-2d.cc
+++ main/compilation-tests/multiple-parameters-2d.cc
@@ -30,4 +30,15 @@ int main() {
         }
         std::cout<<std::endl;
     }
+    {
+        LoggerProgress logger("Monte-Carlo per-bin parallel");
+        std::vector<std::vector<float>> sol(bins,std::vector<float>(bins,0.0f));
+        integrate(integrator_per_bin_parallel(monte_carlo(samples)),sol,f,range_primary<3>(),logger);
+        for (const auto& vv : sol) {
+            for (float v : vv)
+                std::cout<<std::fixed<<std::setprecision(2)<<std::setw(4)<<v<<" ";
+            std::cout<<std::endl;
+        }
+        std::cout<<std::endl;
+    }
 } 
