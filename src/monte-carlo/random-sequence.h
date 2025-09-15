56f7f25ccda4499f82e88126e4e01fda9b7de388
Adolfo <adolfo@unizar.es>
2025-09-15 12:02:01 +0200

    Bug in using default random sequence.

--- src/monte-carlo/random-sequence.h
+++ src/monte-carlo/random-sequence.h
@@ -7,7 +7,7 @@ namespace viltrum {
 
 template<typename Number,typename RNG = std::mt19937>
 auto random_sequence(const RangeInfinite<Number>& r, std::size_t seed = std::random_device()()) {
-    return random_sequence_seed(seed,r);
+    return random_sequence_seed(r,seed);
 } 
 
 
