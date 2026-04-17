c2e7edc47a09f3368e9d0e53d47b3a80bce26ed3
Adolfo <adolfo@unizar.es>
2026-04-17 13:38:17 +0200

    Testing random sequences.

--- main/compilation-tests/random-sequence.cc
+++ main/compilation-tests/random-sequence.cc
@@ -1,16 +1,41 @@
-#include "../../src/monte-carlo/random-sequence.h"
+#include "../../src/monte-carlo/random-sequence-ref.h"
+#include "../../src/monte-carlo/random-sequence-rng.h"
+#include "../../src/monte-carlo/random-sequence-seed.h"
+#include "../../src/rng/xoroshiro.h"
 #include <iostream>
+#include <chrono>
 
 
 
 using namespace viltrum;
 
+
+template<typename Seq>
+void test_sequence(const char* name, const Seq& seq, unsigned long n = 10000000) {
+    unsigned long i = 0;
+    float sum = 0.0f;
+
+    auto t0 = std::chrono::high_resolution_clock::now();
+    for (float x : seq) {
+        sum += x;
+        if (++i >= n) break;
+    }
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+    double elapsed = std::chrono::duration<double>(t1 - t0).count();
+    std::cout << name<<"  - "<<sum << "  (elapsed: " << elapsed << " s)" << std::endl;
+}
+
 int main() {
-    auto l = random_sequence(RangeInfinite(std::vector{-2.0},std::vector{2.0}));
-    int i = 0;
-    for (double d : l) {
-        std::cout<<d<<" ";
-        if (++i>=10) break;
-    } 
+    RangeInfinite<float> range(std::vector{-2.0f},std::vector{2.0f});
+    test_sequence("Seed - mt19937",random_sequence_seed(range,0));
+    test_sequence("RNG  - mt19937",random_sequence_rng(range,0));
+    std::mt19937 rng(0);
+    test_sequence("Ref  - mt19937",random_sequence_ref(range,rng));
+    
+    test_sequence("Seed - xoros. ",random_sequence_seed<float,Xoroshiro128Plus>(range,0));
+    test_sequence("RNG  - xoros. ",random_sequence_rng<float,Xoroshiro128Plus>(range,0));
+    Xoroshiro128Plus rng2(0);
+    test_sequence("Ref  - xoros. ",random_sequence_ref(range,rng2));
     std::cout<<std::endl;
 }  
